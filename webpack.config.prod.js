1c6afda004f6b049c84ab9e98fb77b5dc6be842d
itaibaruch <itai.baruch.22@gmail.com>
2016-04-18 00:37:56 +0700

    np map for prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
+  // devtool: 'source-map', 
   entry: [
     './src/index'
   ],
